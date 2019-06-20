15a6a63be0b1ac6785b8f58d3afd97c55fc85c61
Shivani <shivanidhiman1619@gmail.com>
2019-06-20 18:54:47 -0400

    Display Protocol

---
+++ C0751731_MID_TERM_MAD3004/C0751731_MID_TERM_MAD3004/IDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IDisplay.swift
+//  C0751731_MID_TERM_MAD3004
+//
+//  Created by Vijender Singh on 2019-06-20.
+//  Copyright © 2019 Shivani Dhiman. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay
+{
+    func printBill()
+}
