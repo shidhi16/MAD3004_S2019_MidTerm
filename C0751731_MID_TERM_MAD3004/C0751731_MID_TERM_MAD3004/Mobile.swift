9b297dd8cb0f81a10fff258566102094fb9b33fd
Shivani <shivanidhiman1619@gmail.com>
2019-06-20 20:17:03 -0400

    Bill Calculation Function Fields added

--- C0751731_MID_TERM_MAD3004/C0751731_MID_TERM_MAD3004/Mobile.swift
+++ C0751731_MID_TERM_MAD3004/C0751731_MID_TERM_MAD3004/Mobile.swift
@@ -14,15 +14,27 @@ class Mobile : Bill
     var plan : String?
     var mobileNo : Float?
     var internetUsage : Float?
-    var minuteUsage : Int?
+    var minuteUsage : Float?
+    var internetCharge : Float?
+    var minuteCharge : Float?
     
-    init(bId: Int, bDate: Date, bType: String, bTotalAmount: Float, manufacturer : String, plan : String, mobileNo : Int, internetUsage : Internet, minuteUsage : Int)
+    init(bId: Int, bDate: Date, bType: String, bTotalAmount: Float, manufacturer : String, plan : String, mobileNo : Int, internetUsage : Internet, minuteUsage : Float, internetCharge : Float, minuteCharge :  Float)
     {
         self.manufacturer = ""
         self.plan = ""
         self.mobileNo = 0000000000
         self.internetUsage = 0
         self.minuteUsage = 0
+        self.internetCharge = 5.5
+        self.minuteCharge = 1
         super.init(bId: bId , bDate: bDate, bType: bType, bTotalAmount: bTotalAmount)
+    }
+    
+   func calcTotal() -> Float?
+    {
+        var total : Float
+        total =  (internetUsage! * self.internetCharge!) + (minuteUsage! * self.minuteCharge!)
+        self.bTotalAmount = total
+        return total
     }
 }
